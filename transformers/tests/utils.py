17582458a30d0791ad4f2e18d7eb020f1dbc117f
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-06 09:27:59 +0100

    Add a USE_GPU env var to control where tests run.

--- transformers/tests/utils.py
+++ transformers/tests/utils.py
@@ -1,23 +1,27 @@
+import logging
 import os
 import unittest
-
 from distutils.util import strtobool
 
 from transformers.file_utils import _tf_available, _torch_available
 
+logger = logging.getLogger(__name__)
 
-try:
-    run_slow = os.environ["RUN_SLOW"]
-except KeyError:
-    # RUN_SLOW isn't set, default to skipping slow tests.
-    _run_slow_tests = False
-else:
-    # RUN_SLOW is set, convert it to True or False.
+
+def get_bool_from_env_var(name, default):
     try:
-        _run_slow_tests = strtobool(run_slow)
+        value = os.environ[name]
+    except KeyError:
+        return default
+
+    try:
+        return strtobool(value)
     except ValueError:
         # More values are supported, but let's keep the message simple.
-        raise ValueError("If set, RUN_SLOW must be yes or no.")
+        raise ValueError("If set, %s must be yes or no." % name)
+
+
+_run_slow_tests = get_bool_from_env_var("RUN_SLOW", default=False)
 
 
 def slow(test_case):
@@ -25,7 +29,7 @@ def slow(test_case):
     Decorator marking a test as slow.
 
     Slow tests are skipped by default. Set the RUN_SLOW environment variable
-    to a truthy value to run them.
+    to yes to run them.
 
     """
     if not _run_slow_tests:
@@ -57,8 +61,31 @@ def require_tf(test_case):
     return test_case
 
 
+_run_tests_on_gpu = get_bool_from_env_var("USE_GPU", default=None)
+
+
 if _torch_available:
-    # Set the USE_CUDA environment variable to select a GPU.
-    torch_device = "cuda" if os.environ.get("USE_CUDA") else "cpu"
+    import torch
+
+    # Set the CUDA_VISIBLE_DEVICES environment variable to select a specific
+    # GPU if the machine has more than one.
+
+    # Set the USE_GPU environment variable to "no" to prevent tests from
+    # running on the GPU when it's available.
+
+    if _run_tests_on_gpu is None:
+        # USE_GPU isn't set -> detect GPU support and use it automatically.
+        torch_device = "cuda" if torch.cuda.is_available() else "cpu"
+    elif _run_tests_on_gpu:
+        # USE_GPU ="yes" -> detect GPU support and warn if it's unavailable.
+        torch_device = "cuda" if torch.cuda.is_available() else "cpu"
+        if torch_device == "cpu":
+            logger.warning(
+                "USE_GPU is enabled but no GPU is available, "
+                "tests will run on CPU anyway."
+            )
+    else:
+        # USE_GPU="no" -> run tests on the CPU.
+        torch_device = "cpu"
 else:
     torch_device = None
